24d296e41717530d9d6d90c51430bb4ded008011
Enzo <enzocasamasso@gmail.com>
2024-03-04 19:40:48 -0300

    feat: add another lifetime use case

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -1,7 +1,14 @@
+//Simple largest string example
 pub fn largest_string<'a>(term_one: &'a str, term_two: &'a str) -> &'a str {
     if term_one.len() > term_two.len() {
         term_one
     } else {
         term_two
     }
+}
+
+pub fn format_string<'a>(a: &'a str, b: &'a str) -> String {
+    let a_string = String::from(a);
+    let b_string = String::from(b);
+    format!("{} {}", a_string, b_string).to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use lifetimes::largest_string;
+use lifetimes::{format_string, largest_string};
 
 mod asynchronous;
 mod lifetimes;
@@ -6,15 +6,19 @@ mod lifetimes;
 fn main() {
     //lifetimes
     let lagerst_str: &str;
+    let format_str_string: String;
     let a_string = String::from("One string");
     let b_string = String::from("Another string");
     {
         let string_one = String::from("Hello");
-        let string_two = String::from("Hello");
+        let string_two = String::from("World!");
         //We'll have a problem 'borrowed value does not live long enough'
         //using 'string_one' and 'string_two'
         lagerst_str = largest_string(&a_string, &b_string);
+        //Here the value is not returning lifetime str
+        format_str_string = format_string(&string_one, &string_two);
     }
     println!("{}", lagerst_str);
+    println!("Format string: {}", format_str_string);
     //end lifetimes
 }
